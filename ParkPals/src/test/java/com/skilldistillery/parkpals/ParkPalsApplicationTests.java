0d5551ea6163f67fd0b2fbb19432acdd9b5fdc47
Justin Martz <justinmartz@protonmail.com>
2023-07-20 16:23:09 -0600

    Create Spring Boot MVC project (ParkPals), add JPA project dependency

---
+++ ParkPals/src/test/java/com/skilldistillery/parkpals/ParkPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.parkpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkPalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
