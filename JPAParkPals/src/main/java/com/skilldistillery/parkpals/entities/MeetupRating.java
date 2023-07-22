421bf4c7818396cad8ad816a625334d001dbaddd
fathsam82 <fathsam82@gmail.com>
2023-07-22 14:58:38 -0600

    meetup rating entity test

---
+++ JPAParkPals/src/main/java/com/skilldistillery/parkpals/entities/MeetupRating.java
@@ -0,0 +1,97 @@
+package com.skilldistillery.parkpals.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "meeetup_rating")
+public class MeetupRating {
+	
+	@EmbeddedId
+	private MeetupRatingId id;
+	private int rating;
+
+	private String comment;
+
+	@Column(name = "create_date")
+	@CreationTimestamp
+	private LocalDateTime createDate;
+
+	@Column(name = "last_update")
+	@UpdateTimestamp
+	private LocalDateTime lastUpdate;
+
+	public MeetupRatingId getId() {
+		return id;
+	}
+
+	public void setId(MeetupRatingId id) {
+		this.id = id;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public String toString() {
+		return "MeetupRating [id=" + id + ", rating=" + rating + ", comment=" + comment + ", createDate=" + createDate
+				+ ", lastUpdate=" + lastUpdate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MeetupRating other = (MeetupRating) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ JPAParkPals/src/main/java/com/skilldistillery/parkpals/entities/MeetupRatingId.java
@@ -0,0 +1,83 @@
+package com.skilldistillery.parkpals.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+
+public class MeetupRatingId implements Serializable{
+	
+	private static final long serialVersionUID = -3674082374943312242L;
+
+	@Column(name = "user_id")
+	private int userId;
+	
+	@Column(name = "meetup_id")
+	private int meetupId;
+
+	public MeetupRatingId() {
+	}
+	
+	public MeetupRatingId(int userId, int meetupId) {
+		this.userId = userId;
+		this.meetupId = meetupId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getMeetupId() {
+		return meetupId;
+	}
+
+	public void setMeetupId(int meetupId) {
+		this.meetupId = meetupId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "MeetupRatingId [userId=" + userId + ", meetupId=" + meetupId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(meetupId, userId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MeetupRatingId other = (MeetupRatingId) obj;
+		return meetupId == other.meetupId && userId == other.userId;
+	}
+	
+	
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// user id, meetup_id, comment, rating, create_date, last_update
