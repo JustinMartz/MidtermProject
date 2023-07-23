7d9d161dbd1f216d8f304cce2c4d750de11ca477
Cortnie Barrow <mommy24227@gmail.com>
2023-07-22 21:25:03 -0400

    added friends

--- DB/parkpalsdb.sql
+++ DB/parkpalsdb.sql
@@ -524,9 +524,12 @@ COMMIT;
 START TRANSACTION;
 USE `parkpalsdb`;
 INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (1, 1);
+INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (1, 2);
+INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (1, 3);
+INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (1, 4);
 INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (2, 1);
-INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (3, 1);
-INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (4, 1);
+INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (2, 2);
+INSERT INTO `user_friend` (`user_id`, `friend_id`) VALUES (2, 3);
 
 COMMIT;
 
